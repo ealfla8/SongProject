e1e6703cfab05da6c8a0dc3c4b40e24b9d8b3458
ealfla8 <68116219+ealfla8@users.noreply.github.com>
2021-11-09 15:59:08 -0500

    Create Song.cpp

---
+++ Song.cpp
@@ -0,0 +1,43 @@
+#include <iostream> //check what I need iostream for 
+#include <string> 
+#include "Song.h"
+using namespace std; 
+
+
+Song::Song(string songTitle, string artistName, string album, string genre, string favLyric)
+{
+    this->songTitle = songTitle; 
+    this->artistName = artistName; 
+    this->album = album; 
+    this->genre = genre; 
+    this->favLyric = favLyric; 
+}
+
+string Song::getSongTitle(){
+    return songTitle; 
+}
+
+string Song::getArtistName(){
+    return artistName; 
+}
+
+string Song::getAlbumName(){
+    return album; 
+}
+
+string Song::getGenre(){
+    return genre; 
+}
+
+string Song::getFavLyric(){
+    return favLyric; 
+}
+
+void Song::PrintInfo(){
+    cout << "Title: " << songTitle << endl; 
+    cout << "Artist Name: " << artistName << endl; 
+    cout << "Album: " << album << endl; 
+    cout << "Genre: " << genre << endl; 
+    cout << "Favorite Lyric: " << favLyric << endl; 
+    cout << endl; 
+} 
