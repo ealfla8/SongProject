850cbe61568b2f1d7a48c646d60dbf97f646e479
ealfla8 <68116219+ealfla8@users.noreply.github.com>
2021-11-09 15:58:46 -0500

    Create Song.h

---
+++ Song.h
@@ -0,0 +1,30 @@
+//Song.h file 
+
+#pragma once 
+#include <iostream> //check what I need iostream for 
+#include <string> 
+using namespace std; 
+
+class Song{
+    private: 
+        string songTitle; 
+        string artistName; 
+        string album; 
+        string genre; 
+        string favLyric; 
+
+    public: 
+
+        //Constructor
+        Song(string songTitle, string artistName, string album, string genre, string favLyric); 
+
+        //Accesor Functions
+        string getSongTitle(); 
+        string getArtistName(); 
+        string getAlbumName(); 
+        string getGenre();
+        string getFavLyric(); 
+
+        void PrintInfo(); 
+    
+}; 
