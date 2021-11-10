37f49053988020b64760d8b752b4ce8ad2998844
ealfla8 <68116219+ealfla8@users.noreply.github.com>
2021-11-10 15:35:10 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -47,13 +47,21 @@ void GetDataFromFile(string filePath, vector<Song>& songs){
 } 
 
 void ListAllSongs(vector<Song>& songs){
-    for(int i=0; i<songs.size(); i++){ //Why does it say I have one more song than I have... 
+    for(int i=0; i<songs.size(); i++) //Why does it say I have one more song than I have... 
         songs.at(i).PrintInfo(); 
+}
+
+void FindArtist(vector<Song>& songs, string artist){
+    cout << "Songs from " << artist << ":" << endl;  //should print out something if artist not found
+    for(int i=0; i<songs.size(); i++){
+        if(songs.at(i).getArtistName() == artist){
+            string title = songs.at(i).getSongTitle(); 
+            cout << title << endl;
+        }
     }
 }
 
 int main(){
-
 
     vector<Song> songs; 
     GetDataFromFile("Songs.csv", songs); 
@@ -61,14 +69,22 @@ int main(){
     cout << "Type in the number of the choice you would like. " << endl; 
     
     cout << "1. List all songs with their info." << endl; 
+    cout << "2. List all songs of a particular artist." << endl; 
 
     int input;
     cin >> input; 
     if(input == 1){
         ListAllSongs(songs); 
+    } 
+    if (input ==2){
+        cout << "Type the artist name." << endl; 
+        string artist; 
+        cin >> artist; //fix this so it works with names with more than one word
+        FindArtist(songs,artist); 
     }
 
-    cout << "The number of songs is " << songs.size() << "." << endl; //Currently, I have 10 songs.. It says I have 11 songs... 
+
+    //cout << "The number of songs is " << songs.size() << "." << endl; //Currently, I have 10 songs.. It says I have 11 songs... 
     
     
 }
