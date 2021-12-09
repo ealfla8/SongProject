33c86b3fb8471dedc6cfc2c4376fb4ae647bcece
ealfla8 <68116219+ealfla8@users.noreply.github.com>
2021-12-09 12:56:16 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,16 @@
 #include <string> 
 #include <sstream>
 #include <algorithm>
+#include <map>
+#include <set>
 
 #include "Song.h"
 
 using namespace std; 
 
-void GetDataFromFile(string filePath, vector<Song>& songs){
+vector<Song> GetDataFromFile(string filePath){
     ifstream inFile(filePath); 
+    vector<Song> songs; 
 
     if(inFile.is_open()){
 
@@ -45,6 +48,7 @@ void GetDataFromFile(string filePath, vector<Song>& songs){
     {
         cout << "File could not be opened" << endl; 
     }
+    return songs; 
 } 
 
 void ListAllSongs(vector<Song>& songs){
@@ -77,34 +81,58 @@ void Alphabetize(vector<Song>& songs){
     sort(songs.begin(), songs.end());
 }
 
+map<string, int> ListAllArtists(vector<Song>& songs){
+    map<string,int> artistNames; 
+    string name; 
+    for(int i=0; i<songs.size(); i++){
+        name = songs.at(i).getArtistName(); 
+        if(artistNames.find(name) == artistNames.end()){ //the name is not in the set yet
+            artistNames.emplace(name,0); 
+        }   
+    }
+    return artistNames;
+}
+
+void SettingArtistNameMap(map<string, int>& artistNameMap, vector<Song>&songs){
+    
+}
+void PrintArtistNameMap(map<string,int>& artistNameMap){ 
+    map<string,int>::iterator iter;  
+    for(iter = artistNameMap.begin(); iter != artistNameMap.end(); iter++){
+        cout << "Artist Name: " << iter->first << endl; 
+        cout << "Number of songs: " << iter->second << endl; 
+    }
+}
+
+
 int main(){
 
-    vector<Song> songs; 
-    vector<string> songTitles; 
+    vector<Song> songs = GetDataFromFile("Songs.csv"); 
+    Alphabetize(songs); 
+    map<string,int> artistNameMap = ListAllArtists(songs); 
 
-    GetDataFromFile("Songs.csv", songs); 
 
     cout << "Type in the number of the choice you would like. " << endl; 
     
-    cout << "1. List all songs with their info." << endl; 
+    cout << "1. List all songs in alphabetical order with their info." << endl; 
     cout << "2. List all songs of a particular artist." << endl; 
-    cout << "3. Alphabetize the song titles." << endl; 
-
+    cout << "3. List the number of songs that are by each artist." << endl;  //I should make a map??..
+    
     int input;
     cin >> input; 
     if(input == 1){
         ListAllSongs(songs); 
     } 
-    if (input ==2){
+    else if (input ==2){
         cout << "Type the artist name." << endl; 
         string artist; 
         cin.ignore();
         getline(cin, artist); 
         FindArtist(songs,artist); 
     } 
-    if(input == 3){
-        Alphabetize(songs); 
-        PrintSongTitles(songs); 
-    }
+    else if (input ==3){
+        PrintArtistNameMap(artistNameMap); 
+    } 
+  
     
 }
